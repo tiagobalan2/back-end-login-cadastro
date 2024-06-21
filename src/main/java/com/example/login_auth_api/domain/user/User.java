7724028f94ab72fde1260a21ac5339ed17e6dd42
tiagobalan <tiagobalan@edu.unifil.br>
2024-06-21 11:25:22 -0300

    first commit

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityFilter.java
@@ -0,0 +1,55 @@
+package com.example.login_auth_api.infra.security;
+
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.antlr.v4.runtime.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    // essa classe vai rodar uma vez a cada requisicao
+// vai fazer a verificação se o token que o usuario mandou esta valido
+// se é um token que a nossa aplicação emitiu
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if(login != null){
+            User user = userRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("User Not Found"));
+            var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    // recebe o request do usuario e pega o header authorization que é onde vai estar o token
+// se nao tiver nada no header authorization, ou seja, nao tem token, eu retorno nulo
+// se tiver eu retorno o authHeader substituindo o bearer por vazio pra ficar só o valor do token
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
+
+
